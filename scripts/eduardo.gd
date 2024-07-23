9431263b1487fb0351b4032927c1c43a3f6679a2
eduardo ferreira <ferreirareal11@gmail.com>
2024-07-23 14:09:13 -0300

    feature/refactor: adicionando area de interacao com itens, removendo aceleracao da movimentacao

--- scripts/eduardo.gd
+++ scripts/eduardo.gd
@@ -2,59 +2,59 @@ extends CharacterBody2D
 
 const VELOCIDADE_NORMAL = 50
 const VELOCIDADE_CORRENDO = 150
-const ACELERACAO = 750
-const FRICCAO = 750
 const TEMPO_CAIDO = 120
 const ESTAMINA_MAXIMA = 300
 
 var estamina = ESTAMINA_MAXIMA
 var caido = TEMPO_CAIDO
 var sentido = [0, 1]
-
-@onready var animação = get_node("animação")
+ 
+@onready var animacao = get_node("animacao")
 
 func _physics_process(delta):
 	
 	var shiftPressionado = Input.is_key_pressed(KEY_SHIFT)
-	var entrada = pega_entrada()
+	var entrada = pega_movimento_x_y()
 
 
 	if estamina < ESTAMINA_MAXIMA and not (shiftPressionado or caido < TEMPO_CAIDO):
 		estamina += 1
 	
 	if entrada != Vector2.ZERO and caido == TEMPO_CAIDO:
-		sentido = [velocity.x, velocity.y] # CONFIA
+		sentido = [velocity.x, velocity.y]
 		
 		if shiftPressionado:
-			animação.play("idle_left")
+			animacao.play("idle_left")
 			if estamina > 0:
-				corre(delta, entrada)
+				corre(entrada)
 			else:
 				cansado()
 		else:
-			animação.play("idle_right")
-			anda(delta, entrada)
+			animacao.play("idle_right")
+			anda(entrada)
 			
 	else:
 		if caido < TEMPO_CAIDO:
 			caido += 1
 			
-		velocity = velocity.move_toward(Vector2.ZERO , FRICCAO * delta)
+		#define velocidade dos dois eixos como 0 caso nao haja input 
+		velocity.x = move_toward(velocity.x, 0, VELOCIDADE_NORMAL)
+		velocity.y = move_toward(velocity.y, 0, VELOCIDADE_NORMAL)
 		
-	anima(velocity.length()) # CONFIA
+	anima(velocity.length())
 	move_and_slide()
 
-func corre(delta, entrada):
-	velocity = velocity.move_toward(entrada * VELOCIDADE_CORRENDO, ACELERACAO * delta)
+func corre(entrada):
+	velocity = velocity.move_toward(entrada * VELOCIDADE_CORRENDO, VELOCIDADE_CORRENDO)
 	estamina -= 1
 
-func anda(delta, entrada):
-	velocity = velocity.move_toward(entrada * VELOCIDADE_NORMAL, ACELERACAO * delta)
+func anda(entrada):
+	velocity = velocity.move_toward(entrada * VELOCIDADE_NORMAL, VELOCIDADE_NORMAL)
 
 func cansado():
 	caido = 0
 
-func pega_entrada():
+func pega_movimento_x_y():
 	var entrada = Vector2.ZERO
 	entrada.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	entrada.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -84,4 +84,7 @@ func anima(modulo_velocidade):
 	else:
 		linha += "_left"
 
-	animação.play(linha)
+	animacao.play(linha)
+
+func morre():
+	queue_free()
