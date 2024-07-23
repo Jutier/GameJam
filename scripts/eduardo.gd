1b002b01fbe1b9daf80c72a2f8c90bbb297b5c09
Jutier <ricardo.10.junior@gmail.com>
2024-07-22 22:34:12 -0300

    Feature:  Stamina

--- scripts/eduardo.gd
+++ scripts/eduardo.gd
@@ -1,24 +1,54 @@
 extends CharacterBody2D
 
-const VELOCIDADE_NORMAL= 120
-const VELOCIDADE_CORRENDO= 150
+const VELOCIDADE_NORMAL = 50
+const VELOCIDADE_CORRENDO = 150
 const ACELERACAO = 750
 const FRICCAO = 750
-var momento = Vector2.ZERO
+const TEMPO_CAIDO = 120
+const ESTAMINA_MAXIMA = 300
+
+var estamina = ESTAMINA_MAXIMA
+var caido = TEMPO_CAIDO
 
 func _physics_process(delta):
-	var entrada = Vector2.ZERO
-	var shiftPressionado = Input.is_key_pressed(KEY_SHIFT)
-	var velocidadeDudu = VELOCIDADE_CORRENDO if shiftPressionado else VELOCIDADE_NORMAL
+	print(estamina, caido)
 	
+	var shiftPressionado = Input.is_key_pressed(KEY_SHIFT)
+	var entrada = pega_entrada()
+
+	
+	if estamina < ESTAMINA_MAXIMA and not (shiftPressionado or caido < TEMPO_CAIDO):
+		estamina += 1
+	
+	if entrada != Vector2.ZERO and caido == TEMPO_CAIDO:
+		if shiftPressionado:
+			if estamina > 0:
+				corre(delta, entrada)
+			else:
+				cansado()
+		else:
+			anda(delta, entrada)
+			
+	else:
+		if caido < TEMPO_CAIDO:
+			caido += 1
+			
+		velocity = velocity.move_toward(Vector2.ZERO , FRICCAO * delta)
+	move_and_slide()
+
+func corre(delta, entrada):
+		velocity = velocity.move_toward(entrada * VELOCIDADE_CORRENDO, ACELERACAO * delta)
+		estamina -= 1
+
+func anda(delta, entrada):
+	velocity = velocity.move_toward(entrada * VELOCIDADE_NORMAL, ACELERACAO * delta)
+
+func cansado():
+	caido = 0
+	
+func pega_entrada():
+	var entrada = Vector2.ZERO
 	entrada.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	entrada.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	entrada = entrada.normalized()
-	# se a stamina acabar e continuar correndo o player cai e fica 0.5 segundos sem andar
+	return entrada.normalized()
 	
-	if entrada != Vector2.ZERO:
-		momento = momento.move_toward(entrada * velocidadeDudu, ACELERACAO * delta)
-	else:
-		momento = momento.move_toward(Vector2.ZERO , FRICCAO * delta)
-		
-	move_and_collide(momento * delta)
