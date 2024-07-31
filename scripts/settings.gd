05fa9cd78ebc14e5a584fe78c1eaaaa8156780ae
Jutier <36769570+Jutier@users.noreply.github.com>
2024-07-30 23:55:24 -0300

    too much, idk

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,11 +1,19 @@
 extends Control
 
+@export var bus_name : String
+var bus_index
+
+func _ready():
+	bus_index = AudioServer.get_bus_index(bus_name)
 
 
 func _on_h_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(0, value) # Replace with function body.
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 
 func _on_back_button_pressed():
+	self.visible = false
+
+
+func _on_menu_button_pressed():
 	LoadingScene.transition("res://scenes/mainMenu.tscn")
-	
