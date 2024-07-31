8330cd63de4204f642931b8bb291b697c9897f7a
Jutier <ricardo.10.junior@gmail.com>
2024-07-31 05:45:53 -0300

    last minute

--- scripts/pieces.gd
+++ scripts/pieces.gd
@@ -1,17 +1,28 @@
 extends Node2D
 
+#@export_group("Parts Properties")
+@export_enum('0', '1', '2') var strength: int
+@export_enum('0', '1', '2') var malleability: int
+@export_enum('0', '1', '2') var cost: int
+@export_enum('0', '1', '2') var mass: int
+
+const SHADOWS = ['#80e5ff', '#eaff80', '#ff80df']
+@onready var props = [strength, malleability, cost, mass]
+
+@onready var viewrect = get_viewport().get_visible_rect()
 
 var player_inside = false
 var pickedUp = false
-@onready var viewrect = get_viewport().get_visible_rect()
+@onready var Dudu = $"../eduardo"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	SignalManager.connect("glasses_on_signal", _on_eduardo_glasses_on_signal)
+	SignalManager.connect("glasses_off_signal", _on_eduardo_glasses_off_signal)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+		pass
 
 func _on_interaction_zone_body_entered(body):
 	if body.has_method("interact"):
@@ -26,9 +37,19 @@ func _on_interaction_zone_body_exited(body):
 func pickUp(body):
 	pickedUp = body
 	self.position = viewrect.end - Vector2(70, 70)
-	self.scale = Vector2(0.7, 0.7)
+	self.scale = Vector2(0.8, 0.8)
 
 func dropDown():
 	self.position = pickedUp.position
 	self.scale = Vector2(1, 1)
 	pickedUp = false
+
+func _on_eduardo_glasses_on_signal(gType):
+	self.modulate = Color(SHADOWS[props[gType]])
+
+
+func _on_eduardo_glasses_off_signal():
+	self.modulate = Color('#ffffff')
+
+func rocketP():
+	pass
