5170d64b780d9138cb9574013c9514a8788f77a9
eduardo ferreira <ferreirareal11@gmail.com>
2024-07-31 00:22:23 -0300

    fix: volume slider

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,16 +1,5 @@
 extends Control
 
-@export var bus_name : String
-var bus_index
-
-func _ready():
-	bus_index = AudioServer.get_bus_index(bus_name)
-
-
-func _on_h_slider_value_changed(value):
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
-
-
 func _on_back_button_pressed():
 	self.visible = false
 
